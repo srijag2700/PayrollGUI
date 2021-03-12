dc83d7f213ee593bae19097bdaedb1d6f4efb3ea
Srija Gottiparthi <srija.gottiparthi@gmail.com>
2021-03-11 19:54:47 -0500

    changed xml file name

--- src/Project3/Main.java
+++ src/Project3/Main.java
@@ -20,7 +20,7 @@ public class Main extends Application {
      */
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("View.fxml"));
         primaryStage.setTitle("Payroll Processing System");
         primaryStage.setScene(new Scene(root, 600, 600));
         primaryStage.show();
